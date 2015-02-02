ea3a50aa60742e8fc243c56e32c018f320d2b667
Ninjabear <edward.richard.lewis@gmail.com>
2015-02-02 11:40:46 +0000

    converted main to fx application

--- src/main/java/speedr/Main.java
+++ src/main/java/speedr/Main.java
@@ -3,11 +3,30 @@
  */
 package speedr;
 
-public class Main {
-    
+import javafx.fxml.FXMLLoader;
+
+import java.lang.Override;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main_window.fxml"));
+
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("speedr");
+
+        stage.setOnCloseRequest((t)->{Platform.exit(); System.exit(0); }); // this is just a shim for something that isn't working properly
+
+        stage.show();
+
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("hello world");
+        launch(args);
     }
     
     
