9fae9cf9c00ffe9c475d9b07628f5f8907844595
mjw <mjw@commsoft.co.uk>
2015-02-03 16:54:12 +0000

    Implement POP3 email sourcing.

---
+++ src/main/java/speedr/sources/email/POP3Inbox.java
@@ -0,0 +1,74 @@
+package speedr.sources.email;
+
+import com.sun.mail.pop3.POP3SSLStore;
+
+import javax.mail.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class POP3Inbox {
+
+    private Store store;
+
+    public POP3Inbox(String host, String user, String pass) {
+
+        String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+
+        Properties pop3Props = new Properties();
+
+        pop3Props.setProperty("mail.pop3.socketFactory.class", SSL_FACTORY);
+        pop3Props.setProperty("mail.pop3.socketFactory.fallback", "false");
+        pop3Props.setProperty("mail.pop3.port",  "995");
+        pop3Props.setProperty("mail.pop3.socketFactory.port", "995");
+
+        URLName url = new URLName("pop3", host, 995, "", user, pass);
+
+        Session mailSession = Session.getInstance(pop3Props, null);
+
+        try {
+            store = new POP3SSLStore(mailSession, url);
+            store.connect();
+        } catch (javax.mail.MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public Store getStore(){
+        return this.store;
+    }
+
+    public Email getLastMessage(){
+        return this.getRecentMessages(0).get(0);
+    }
+
+    public List<Email> getRecentMessages(int number) {
+
+        List<Email> out = new ArrayList<>();
+
+        try {
+
+            Folder f = this.store.getFolder("INBOX");
+            f.open(Folder.READ_ONLY);
+
+            int max = f.getMessageCount();
+            int min = max-number < 1 ? 1 : max-number;
+
+            for(Message m : f.getMessages(min, max)){
+                // todo: multipart email parsing.
+                out.add(new Email(m.getFrom()[0].toString(), m.getSubject(), m.getContent().toString()));
+            }
+
+            f.close(false);
+
+        } catch (MessagingException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return out;
+
+    }
+
+}
--- src/test/java/speedr/core/GmailTest.java
+++ src/test/java/speedr/core/GmailTest.java
@@ -9,15 +9,31 @@ package speedr.core;
 import org.junit.Test;
 import speedr.sources.email.Email;
 import speedr.sources.email.IMAPInbox;
+import speedr.sources.email.POP3Inbox;
 
 import static org.junit.Assert.assertTrue;
 
 public class GmailTest {
 
     @Test
-    public void GmailTest() throws Exception {
+    public void IMAPTest() throws Exception {
 
         IMAPInbox inbox = new IMAPInbox("imap.gmail.com", "speedrorg@gmail.com", "speedrspeedr");
+
+        assertTrue(inbox.getStore().isConnected());
+
+        for(Email e : inbox.getRecentMessages(1)){
+            assertTrue(e.getFrom() != null);
+            assertTrue(e.getSubject() != null);
+            assertTrue(e.getContent() != null);
+        }
+
+    }
+
+    @Test
+    public void POP3Test() throws Exception {
+
+        POP3Inbox inbox = new POP3Inbox("pop.gmail.com", "speedrorg@gmail.com", "speedrspeedr");
 
         assertTrue(inbox.getStore().isConnected());
 
