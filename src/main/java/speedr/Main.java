b2ce13c637206081088db56097ed163e39862167
Roo <richard.h.ingram@gmail.com>
2015-02-03 15:12:24 +0000

    #6 - Beginnings of layout for UI; merged enhancements to mock UI and dropped mock UI.

--- src/main/java/speedr/Main.java
+++ src/main/java/speedr/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mock_window.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main_window.fxml"));
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
--- src/main/java/speedr/gui/controllers/MainWindowController.java
+++ src/main/java/speedr/gui/controllers/MainWindowController.java
@@ -1,28 +1,81 @@
 package speedr.gui.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
+import speedr.core.SpeedReadEventPump;
+import speedr.core.SpeedReaderStream;
+import speedr.core.listeners.WordPumpEvent;
+import speedr.core.listeners.WordPumpEventListener;
+import speedr.sources.email.Email;
+import speedr.sources.email.IMAPInbox;
 
-import javafx.event.ActionEvent;
-
-/**
+/**r
  *
  * Controller for the Speed Reader main panel GUI.
  *
  */
 
-public class MainWindowController {
+public class MainWindowController implements WordPumpEventListener {
+
+    private SpeedReadEventPump pump;
+
+    private boolean startedReading = false;
 
     @FXML
-    private Button goBtn;
-    @FXML
-    private Label lbl;
+    private Label promptLabel;
 
     @FXML
-    public void goBtnAction(ActionEvent evt)
-    {
-        lbl.setText(lbl.getText()+" fart");
+    private Label currentWordLabel;
+
+    @FXML
+    private Text finishText;
+
+    @FXML
+    public void onKeyPressed(KeyEvent keyEvent) {
+
+        if (!startedReading && (keyEvent.getCode() == KeyCode.P)) {
+
+            startedReading = true;
+
+            promptLabel.setVisible(false);
+            currentWordLabel.setVisible(true);
+
+            // get an email
+            IMAPInbox inbox = new IMAPInbox("imap.gmail.com", "speedrorg@gmail.com", "speedrspeedr");
+            Email e = inbox.getLastMessage();
+
+            // set up a speed reading stream from the email.
+            SpeedReaderStream s = new SpeedReaderStream(e);
+
+            // the pump lets us plug the stream into our gui
+            pump = new SpeedReadEventPump(s, 700);
+            pump.addWordPumpEventListener(this);
+
+            // kick it off
+            pump.start();
+        }
     }
 
+    @FXML
+    public void onConfigureButtonClick() {
+
+        //ToDo: Settings
+    }
+
+    @Override
+    public void wordPump(WordPumpEvent wordPumpEvent) {
+        if (wordPumpEvent.isDone())
+        {
+            // pump.removeWordPumpEventListener(this);
+            // annoyingly can't do this because we're using the listener list to do the iteration for this callback
+            // unless i take a copy
+            // TBC
+            finishText.setVisible(true);
+        } else {
+            currentWordLabel.setText(wordPumpEvent.getWord().asText());
+        }
+    }
 }
