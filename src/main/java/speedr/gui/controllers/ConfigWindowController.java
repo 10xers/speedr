027fc66a53ed4662b340042f59a74d9c4267433d
Ninjabear <edward.richard.lewis@gmail.com>
2015-02-03 23:16:36 +0000

    added config window stub

---
+++ src/main/java/speedr/gui/controllers/ConfigWindowController.java
@@ -0,0 +1,23 @@
+package speedr.gui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * Speedr / Ed
+ * 03/02/2015 22:55
+ */
+public class ConfigWindowController {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    public void cancelButtonClicked(ActionEvent actionEvent) {
+        Stage us = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        us.close();
+    }
+}
--- src/main/java/speedr/gui/controllers/MainWindowController.java
+++ src/main/java/speedr/gui/controllers/MainWindowController.java
@@ -6,13 +6,20 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import speedr.core.SpeedReadEventPump;
 import speedr.core.SpeedReaderStream;
 import speedr.core.listeners.WordPumpEvent;
@@ -20,6 +27,7 @@ import speedr.core.listeners.WordPumpEventListener;
 import speedr.sources.email.Email;
 import speedr.sources.email.IMAPInbox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -111,9 +119,23 @@ public class MainWindowController implements WordPumpEventListener, Initializabl
     }
 
     @FXML
-    public void onConfigureButtonClick() {
+    public void onConfigureButtonClick(ActionEvent evt) {
+        Parent root;
 
-        //ToDo: Settings
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/config_window.fxml"));
+        } catch (IOException e) {
+            throw new IllegalStateException("failed loading fxml for config window", e);
+        }
+
+        Stage s = new Stage();
+        s.setTitle("Configuration");
+        s.setScene(new Scene(root));
+
+        s.initModality(Modality.APPLICATION_MODAL);
+        s.initOwner(((Node)evt.getSource()).getScene().getWindow() );
+        s.show();
+
     }
 
     private void loadEmails() {
