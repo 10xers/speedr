6871977de8f3f69667838a05e5ad3b0a0cb806a3
mjw <mjw@commsoft.co.uk>
2015-02-04 23:44:12 +0000

    Sploosh.

--- src/main/java/speedr/Main.java
+++ src/main/java/speedr/Main.java
@@ -44,6 +44,5 @@ public class Main extends Application {
     {
         launch(args);
     }
-    
-    
+
 }
--- src/main/java/speedr/gui/controllers/SplashController.java
+++ src/main/java/speedr/gui/controllers/SplashController.java
@@ -1,7 +1,15 @@
 package speedr.gui.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,6 +17,27 @@ public class SplashController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Application started.");
+    }
+
+    @FXML
+    public void onLoginButtonClick(ActionEvent evt){
+
+        System.out.println("logging in");
+
+        Stage currentStage = (Stage) ((Node)evt.getSource()).getScene().getWindow();
+
+        Parent main = null;
+
+        try {
+            main = FXMLLoader.load(getClass().getResource("/fxml/main_window.fxml"));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        Scene s = new Scene(main);
+        s.getStylesheets().add("/style/speedr.css");
+        currentStage.setScene(s);
 
     }
 
